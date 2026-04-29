5744c3b39029cbbe0f9f95248c0f71a2c5d6fece
jorlly-collado-castro <263438472+jorlly-collado-castro@users.noreply.github.com>
2026-04-29 08:32:30 +0200

    feat: Add Microbit.Buttons UI driver

---
+++ src/microbit-buttons.adb
@@ -0,0 +1,31 @@
+with Microbit.Pins;
+
+package body Microbit.Buttons is
+
+   Button_A_Pin : constant Microbit.Pins.Pin_Id := (Microbit.Pins.Port_0, 14);
+   Button_B_Pin : constant Microbit.Pins.Pin_Id := (Microbit.Pins.Port_0, 23);
+
+   procedure Initialize is
+   begin
+      Microbit.Pins.Configure (Button_A_Pin, Mode => Microbit.Pins.Input, Pull => Microbit.Pins.Pull_Up);
+      Microbit.Pins.Configure (Button_B_Pin, Mode => Microbit.Pins.Input, Pull => Microbit.Pins.Pull_Up);
+   end Initialize;
+
+   function State (Button : Button_Id) return Button_State is
+      Pin : Microbit.Pins.Pin_Id;
+   begin
+      case Button is
+         when Button_A =>
+            Pin := Button_A_Pin;
+         when Button_B =>
+            Pin := Button_B_Pin;
+      end case;
+
+      if Microbit.Pins.Read (Pin) then
+         return Released;
+      else
+         return Pressed;
+      end if;
+   end State;
+
+end Microbit.Buttons;
---
+++ src/microbit-buttons.ads
@@ -0,0 +1,11 @@
+package Microbit.Buttons is
+
+   type Button_Id is (Button_A, Button_B);
+
+   type Button_State is (Pressed, Released);
+
+   procedure Initialize;
+
+   function State (Button : Button_Id) return Button_State;
+
+end Microbit.Buttons;
