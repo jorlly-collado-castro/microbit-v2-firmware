d85dcd638583ef0a680beaf405c14c8ea4ad9e22
jorlly-collado-castro <263438472+jorlly-collado-castro@users.noreply.github.com>
2026-04-29 09:22:33 +0200

    style: format sensor log output as CSV-like string

--- examples/sensors/src/sensors.adb
+++ examples/sensors/src/sensors.adb
@@ -22,14 +22,13 @@ begin
       --  Read accelerometer
       Accel_Data := Microbit.Accelerometer.Read_Data;
 
-      --  Print values
-      Microbit.Console.Put ("X: ");
-      Microbit.Console.Put_Line (Integer'Image (Integer (Accel_Data.X)));
-      Microbit.Console.Put ("Y: ");
-      Microbit.Console.Put_Line (Integer'Image (Integer (Accel_Data.Y)));
-      Microbit.Console.Put ("Z: ");
+      --  Print values in a single line (CSV-like) for easy serial plotting
+      Microbit.Console.Put ("X:");
+      Microbit.Console.Put (Integer'Image (Integer (Accel_Data.X)));
+      Microbit.Console.Put (", Y:");
+      Microbit.Console.Put (Integer'Image (Integer (Accel_Data.Y)));
+      Microbit.Console.Put (", Z:");
       Microbit.Console.Put_Line (Integer'Image (Integer (Accel_Data.Z)));
-      Microbit.Console.Put_Line ("-------------------");
 
       --  Wait for next period
       Next_Release := Next_Release + Period;
